5eb87fa1d66715e915a91e0c08779a89ad9d2681
secabhi <abhiatstudy@gmail.com>
2017-08-01 19:41:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+var app = express();
+
+app.get('/',function(req,res){
+res.send({"hi":"there"})
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT);
