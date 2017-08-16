9f6075d93cdd45750de2112f653f63869693417f
Abhishek <abhiatstudy@gmail.com>
2017-08-16 07:01:24 +0530

    commited

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const passport = require('passport');
-const googleAuth = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 const keys = require('./config/keys');
 
-passport.use(new googleAuth({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientId,
+  clientSecret: keys.googleSecretKey,
   callback: '/auth/google/callback'
 }, (accessToken) => {
   console.log(accessToken);
@@ -17,10 +17,7 @@ passport.use(new googleAuth({
 app.get('/auth/google', passport.authenticate('google', {
   scope: ['profile', 'email']
 })
-)
-
-// Client id -- 819880548666-neul5ufjf1b394l7acoq11jlrldcf75k.apps.googleusercontent.com
-// Client secret -- rsmth5HazwWftGO54Sv-iPKV
+);
 
 const PORT = process.env.PORT || 5000
 
