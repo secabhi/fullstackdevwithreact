40ffd1095a6878a5e3972ccdd5ee9d70cdbedef9
Abhishek <abhiatstudy@gmail.com>
2017-08-19 09:25:11 +0530

    updated

--- services/passport.js
+++ services/passport.js
@@ -1,12 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
-    clientID: keys.googleClientId,
-    clientSecret: keys.googleSecretKey,
-    callbackURL: '/auth/google/callback'
-  }, (accessToken) => {
-    console.log(accessToken);
-  }))
-  
+  clientID: keys.googleClientId,
+  clientSecret: keys.googleSecretKey,
+  callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOne({ "googleId": profile.id })
+    .then((existingUser) => {
+      if (existingUser) {
+        done(null, existingUser)
+      } else {
+        console.log(accessToken);
+        console.log("profile:", profile);
+        new User({ googleId: profile.id }).save()
+          .then((user) => {
+            user => done(null, user)
+          })
+      }
+    })
+
+}))
